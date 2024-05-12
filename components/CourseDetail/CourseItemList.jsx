7dc0a87e7e3084bd01e2b86f75831ed78154baa8
ZeynepAksoy <zaksoy852@gmail.com>
2024-05-12 21:19:20 +0300

    Expenditure List Updated

--- components/CourseDetail/CourseItemList.jsx
+++ components/CourseDetail/CourseItemList.jsx
@@ -1,20 +1,31 @@
 import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
+import Colors from '../../utils/Colors'
 
 export default function CourseItemList({categoryData}) {
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Harcamalar</Text>
 
-      <View>
+      <View style={{marginTop:15}}>
         {categoryData?.CategoryItems?.map((item,index)=>(
-            <View key={index}>
+          <>
+            <View key={index} style={styles.itemContainer}>
                 <Image source={{uri:item.image}} style={styles.image}/>
+                <View style={{flex:1, marginLeft:10}}>
+                  <Text style={styles.name}>{item.name}</Text>
+                  <Text style={styles.url}>{item.url}</Text>
+                </View>
+                <Text style={styles.cost}>{item.cost}₺</Text>
             </View>
+            {categoryData?.CategoryItems.length-1!=index&&
+            <View style={{borderWidth:0.5,marginTop:10,borderColor:Colors.GRAY}}></View>
+            }
+            </>
         ))}
       </View>
     </View>
-  )
+  ) 
 }
 
 const styles=StyleSheet.create({
@@ -26,8 +37,28 @@ const styles=StyleSheet.create({
         fontSize:20
     },
     image:{
-        width:100,
-        height:100
+        width:90,
+        height:90,
+        borderRadius:15
 
+    },
+    itemContainer:{
+      display:'flex',
+      flexDirection:'row',
+      justifyContent:'space-between',
+      alignItems:'center'
+    },
+    name:{
+      fontSize:20,
+      fontFamily:'outfit-bold'
+    },
+    url:{
+      fontFamily:'outfit',
+      color:Colors.GRAY
+    },
+    cost:{
+      fontSize:17,
+      marginLeft:10,
+      fontFamily:'outfit-bold'
     }
-})
+}) 
