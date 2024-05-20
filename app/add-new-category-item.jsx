b97ad99c53874b129becba39f2284b46c5cb6039
ZeynepAksoy <zaksoy852@gmail.com>
2024-05-20 17:37:30 +0300

    Expenditure page additions have been made

--- app/add-new-category-item.jsx
+++ app/add-new-category-item.jsx
@@ -1,12 +1,54 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet , Image, TextInput} from 'react-native'
 import React, { useState } from 'react'
+import Colors from '../utils/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
-
+const placeholder='storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png'
 export default function AddNewCategoryItem() {
-    const [ image,setImage]=useState();
+    const [ image,setImage]=useState(placeholder);
   return (
-    <View>
-      <Text>Harcama Ekle</Text>
+    <View style={{padding:20}}>
+      <Image source={{uri:image}}
+      style={styles.image}
+      />  
+      <View style={styles.textInputContainer}>
+        <Ionicons name="pricetag" size={24} color={Colors.GRAY} />
+        <TextInput placeholder='Harcama adı' style={styles.input}/>  
+      </View> 
+      <View style={styles.textInputContainer}>
+        <Ionicons name="pricetag" size={24} color={Colors.GRAY} />
+        <TextInput placeholder='Ücret' style={styles.input}/>  
+      </View> 
+      <View style={styles.textInputContainer}>
+        <Ionicons name="pricetag" size={24} color={Colors.GRAY} />
+        <TextInput placeholder='Url' style={styles.input}/>  
+      </View>  
+      <View style={styles.textInputContainer}>
+        <Ionicons name="pricetag" size={24} color={Colors.GRAY} />
+        <TextInput placeholder='Not' style={styles.input}/>  
+      </View>  
     </View>
   )
 }
+const styles = StyleSheet.create({
+  image:{ 
+    width:150,
+    height:150,
+    backgroundColor:Colors.GRAY,
+    borderRadius:15
+  },
+  textInputContainer:{
+    padding:10,
+    borderWidth:1,
+    display:'flex',
+    flexDirection:'row',
+    gap:10,
+    alignItems:'center',
+    borderRadius:10,
+    borderColor:Colors.GRAY,
+    marginTop:10
+  },
+  input:{
+    fontSize:17
+  }
+})
